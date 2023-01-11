805d3834a29812afdf8bc6a42590618ffa749b39
Quoc Huynh <quoc.huynh@cosee.biz>
2023-01-11 08:38:53 +0100

    Add rect cropper

--- native_image_cropper_ios/example/lib/main.dart
+++ native_image_cropper_ios/example/lib/main.dart
@@ -1,11 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:native_image_cropper_ios/native_image_cropper_ios.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _nativeImageCropperAndroidPlugin = NativeImageCropperIOS();
+  Uint8List? _inputImage;
+  Uint8List? _outputImage;
+
+  @override
+  void initState() {
+    super.initState();
+    initTryOutPlugin();
+  }
+
+  // TODO real example
+  Future<void> initTryOutPlugin() async {
+    final ByteData bytes = await rootBundle.load('assets/test_image.png');
+    Uint8List inputByteList = bytes.buffer.asUint8List();
+    setState(() {
+      _inputImage = inputByteList;
+    });
+
+    Uint8List? croppedByteList =
+        await _nativeImageCropperAndroidPlugin.cropRect(
+            bytes: Uint8List.fromList(inputByteList),
+            x: 200,
+            y: 350,
+            width: 450,
+            height: 450);
+    setState(() {
+      _outputImage = croppedByteList;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +53,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            if (_inputImage != null)
+              Column(children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Original"),
+                ),
+                Image.memory(
+                  _inputImage!,
+                  height: 300,
+                )
+              ]),
+            if (_outputImage != null)
+              Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Cropped"),
+                  ),
+                  Image.memory(
+                    _outputImage!,
+                    height: 200,
+                  ),
+                ],
+              )
+          ],
         ),
       ),
     );
--- native_image_cropper_ios/ios/Classes/SwiftNativeImageCropperIosPlugin.swift
+++ native_image_cropper_ios/ios/Classes/SwiftNativeImageCropperIosPlugin.swift
@@ -1,13 +1,73 @@
 import Flutter
 
 public class SwiftNativeImageCropperPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "biz.cosee/native_image_cropper_ios", binaryMessenger: registrar.messenger())
-    let instance = SwiftNativeImageCropperPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("Test")
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "biz.cosee/native_image_cropper_ios", binaryMessenger: registrar.messenger())
+        let instance = SwiftNativeImageCropperPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+            case "cropCircle":
+                handleCropRect(call,  result)
+            case "cropRect":
+                handleCropRect(call,  result)
+            default:
+                result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func handleCropRect(_ call: FlutterMethodCall,_ result: @escaping FlutterResult){
+        DispatchQueue.main.async {
+            do {
+                let args = call.arguments as! [String: Any]
+                let bytes = args["bytes"] as! FlutterStandardTypedData
+                let x = args["x"] as! Int
+                let y = args["y"] as! Int
+                let width = args["width"] as! Int
+                let height = args["height"] as! Int
+                
+                let image = try self.flutterStandardTypeDataToUIImage(bytes: bytes)
+                let croppedImage = try self.getCroppedUIImage(image: image, x: x, y: y, width: width, height: height)
+                let croppedBytes = try self.uiImageToFlutterStandardTypedData(image: croppedImage)
+                result(croppedBytes)
+            } catch {
+                result(FlutterError.init(code: String(describing: error.self), message: error.localizedDescription, details:nil))
+            }
+        }
+    }
+    
+    private func uiImageToFlutterStandardTypedData(image: UIImage) throws -> FlutterStandardTypedData {
+        let bytes = image.jpegData(compressionQuality: 1)
+        if let bytes {
+            return FlutterStandardTypedData(bytes: bytes)
+        }
+        // TODO: Raise error if nil
+        return FlutterStandardTypedData(bytes: bytes!)
+    }
+    
+    private func flutterStandardTypeDataToUIImage(bytes: FlutterStandardTypedData) throws -> UIImage{
+        let image = UIImage(data: bytes.data)
+        if let image {
+            return image
+        }
+        // TODO Raise exception
+        return image!
+    }
+    
+    private func getCroppedUIImage(image: UIImage,x: Int, y: Int, width: Int, height: Int) throws -> UIImage{
+        let cropRect = CGRect(x: x, y: y, width: width, height: height)
+        let cgImage = image.cgImage
+        let croppedCgImage = cgImage?.cropping(to: cropRect)
+        if let croppedCgImage {
+            return UIImage(
+                cgImage: croppedCgImage,
+                scale: image.imageRendererFormat.scale,
+                orientation: image.imageOrientation
+            )
+        }
+        // TODO throw exception
+        return image
+    }
 }
