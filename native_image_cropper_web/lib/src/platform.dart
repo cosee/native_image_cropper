f64a4ef76165cd001bfd333598e9f79fcb7dab88
Quoc Huynh <quoc.huynh@cosee.biz>
2023-04-12 13:47:19 +0200

    Add color space conversion

--- native_image_cropper_web/lib/src/platform.dart
+++ native_image_cropper_web/lib/src/platform.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:native_image_cropper_platform_interface/native_image_cropper_platform_interface.dart';
@@ -40,7 +40,7 @@ class NativeImageCropperPlugin extends NativeImageCropperPlatform {
         .toImageSync(width, height)
         .toByteData(format: ImageByteFormat.png);
     if (byteData != null) {
-      return Uint8List.view(byteData.buffer);
+      return byteData.buffer.asUint8List();
     } else {
       throw const NativeImageCropperException(
         'Convert image',
@@ -83,17 +83,38 @@ class NativeImageCropperPlugin extends NativeImageCropperPlatform {
         Paint(),
       );
 
-    final byteData = await recorder
-        .endRecording()
-        .toImageSync(width, height)
-        .toByteData(format: ImageByteFormat.png);
+    final croppedImage = recorder.endRecording().toImageSync(width, height);
+    final byteData = await croppedImage.toByteData();
     if (byteData != null) {
-      return Uint8List.view(byteData.buffer);
+      return byteData.buffer.asUint8List();
     } else {
       throw const NativeImageCropperException(
         'Convert image',
         'Could not convert image to Uint8List',
       );
     }
+  }
+}
+
+class Jpg {
+  const Jpg._();
+
+  static Uint8List colorSpaceConversion(Uint8List rgb) {
+    final yCbCr = Uint8List(rgb.length);
+
+    for (int i = 0; i < rgb.length; i += 3) {
+      final r = rgb[i];
+      final g = rgb[i + 1];
+      final b = rgb[i + 2];
+
+      final y = 16 + (r * 0.299 + g * 0.587 + b * 0.114).round();
+      final cb = 128 + (r * -0.168736 - g * 0.331264 + b * 0.5).round();
+      final cr = 128 + (r * 0.5 - g * 0.418688 - b * 0.081312).round();
+      yCbCr[i] = y;
+      yCbCr[i + 1] = cb;
+      yCbCr[i + 2] = cr;
+    }
+
+    return yCbCr;
   }
 }
